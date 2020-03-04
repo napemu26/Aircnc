2e3ce1a6974e6619d99cc68333e1b4efa8412779
Nathan Osti Miguel <napemu26>
2020-03-03 22:41:59 -0300

    Corrigido erro no Escopo

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -7,7 +7,7 @@ module.exports = {
 		let user = await User.findOne({ email })
 
 		if (!user) {
-			const user = await User.create({ email })
+			user = await User.create({ email })
 		}
 
 		return res.json(user)
